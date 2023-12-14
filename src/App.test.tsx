448598f776b939da3bfcd2684c50fc92b873c81c
My Ha Cao <myhadev@gmail.com>
2023-12-13 23:37:47 -0400

    TS-19 Toolbar unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Toolbar from './components/toolbar';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello World!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Toolbar component', () => {
+  it('renders without crashing', () => {
+    render(<Toolbar />);
+  });
 });
